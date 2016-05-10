49e23450ae2b05cee9d8bae1fc58a5699d8f4f43
Bart Kastermans <kasterma@kasterma.net>
2016-05-10 20:09:45 +0200

    use zoo.cfg from vagrant dir

--- zk-install-script.sh
+++ zk-install-script.sh
@@ -1,5 +1,5 @@
 cp /vagrant/zookeeper-3.4.8.tar.gz .
 tar xzf zookeeper-3.4.8.tar.gz
-cp zookeeper-3.4.8/conf/zoo_sample.cfg zookeeper-3.4.8/conf/zoo.cfg
+cp /vagrant/zoo.cfg zookeeper-3.4.8/conf/zoo.cfg
 mkdir -p /tmp/zookeeper
 zookeeper-3.4.8/bin/zkServer.sh start
